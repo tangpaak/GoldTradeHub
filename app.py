f261d45d8e336a175b86d54b3b961758ffb54368
tangpaak <tangpaak@gmail.com>
2024-10-12 01:53:43 +0800

    update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.declarative import declarative_base
 
-class Base(DeclarativeBase):
-    pass
+# Use declarative_base instead of DeclarativeBase
+Base = declarative_base()
 
 db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
